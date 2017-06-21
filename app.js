b20b366e5b5583793d52ea1c822570bb8db4ee09
GummyGuppy <alphastack.dev@gmail.com>
2017-06-21 17:35:25 -0500

    added database

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 
 const express = require('express'),
       ejs = require('ejs'),
-      path = require('path')
+      path = require('path'),
+      mongoose = require('mongoose'),
+      config = require('./config/database')
+
+
+const app = express()
 
 //bring in models
 let poll = require('./models/poll'),
     user = require('./models/user')
 
-const app = express()
 
 
 //set view engine
@@ -16,8 +20,26 @@ app.engine('ejs', require('express-ejs-extend'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+//=== connect to database ============
+mongoose.connect(config.database)
+const db = mongoose.connection
+
+db.on('open', () => {
+  console.log('connected to database')
+})
+
+db.on('error', (err) => {
+  throw err
+})
+
+//======================================
+
 app.get('/', (req, res) => {
   res.render('login')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
 })
 
 //import routes
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: 'mongodb://admin:admin@ds133922.mlab.com:33922/pollit',
+  secret: 'yoursecret'
+}
